ac3f00ad44fc363791ec23e6b9cae3f9b4c3f45b
Ethan Braum <139394301+Yemyam@users.noreply.github.com>
2026-04-22 21:44:23 -0600

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        out = ""
+        for string in strs:
+            out += f"{string}é"
+        return out
+
+    def decode(self, s: str) -> List[str]:
+        out = []
+        out_string = ""
+        for char in s:
+            if char != "é":
+                out_string += char
+            else:
+                out.append(out_string)
+                out_string = ""
+        return out
